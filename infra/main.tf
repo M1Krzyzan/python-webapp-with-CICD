b338a481ea720c46848bc573358d70cbb1c110f6
Michał Krzyżanowski <michal.13777@gmail.com>
2025-02-16 18:29:10 +0100

    added random string to function app name

--- infra/main.tf
+++ infra/main.tf
@@ -49,7 +49,7 @@ resource "azurerm_service_plan" "backend" {
 }
 
 resource "azurerm_linux_function_app" "backend" {
-  name                       = "paim-app-function"
+  name                       = "paim-app-function${random_string.unique.result}"
   location                   = azurerm_resource_group.main.location
   resource_group_name        = azurerm_resource_group.main.name
   storage_account_access_key = azurerm_storage_account.backend.primary_access_key
