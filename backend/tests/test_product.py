04bc44a6a533a6757fa894255b2097a07983f36e
Michał Krzyżanowski <michal.13777@gmail.com>
2025-02-19 03:16:25 +0100

    fixed typo

--- backend/tests/test_product.py
+++ backend/tests/test_product.py
@@ -10,10 +10,10 @@ from application.product.product_service import ProductService
 from application.responses import ProductResponse
 from domain.entities import Entity
 from domain.exceptions import EntityNotFoundError, InvalidDateType
-from infrastructure import app
-from infrastructure import Container
+from infrastructure.api.main import app
+from infrastructure.containers import Container
 from infrastructure.mongo.product_repository import ProductRepositoryMongo
-from application.auth import AuthService
+from application.auth.auth_service import AuthService
 
 
 @pytest.fixture(scope="module")
