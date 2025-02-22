e8043e3c33dd2446943d9abe6df54c309dc13904
Michał Krzyżanowski <michal.13777@gmail.com>
2025-02-22 17:52:17 +0100

    added output for debugging process

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -2,6 +2,10 @@ output "backend_url" {
   value = "https://${azurerm_linux_function_app.backend.default_hostname}"
 }
 
+output "cleaned_frontend_url" {
+  value = local.cleaned_frontend_url
+}
+
 output "frontend_storage_name" {
   value = azurerm_storage_account.frontend.name
 }
